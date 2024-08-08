f7405e14a8b5f6ce2b2ef210a7d5b3f998dab221
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-08-09 04:04:35 +0530

    commit adds amoy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,18 +16,29 @@ const config: HardhatUserConfig = {
       url: `https://sepolia.infura.io/v3/${process.env.API_KEY_SEPOLIA}`,	
       accounts: [process.env.PRIVATE_KEY_SEPOLIA || ''],
     },
+    amoy:{
+      url: `https://polygon-amoy.infura.io/v3/${process.env.API_KEY_AMOY}`,
+      accounts: [process.env.PRIVATE_KEY_AMOY || ''],
+    },
+    cardona: {
+      url: `https://rpc.ankr.com/polygon_zkevm_cardona`,
+      accounts: [process.env.PRIVATE_KEY_CARDONA || ''],
+   
+    },
     // scrollSepolia: {
     //   url: `https://sepolia-rpc.scroll.io/`,
     //   accounts: [process.env.PRIVATE_KEY_SCROLL_SEPOLIA || ''],
     // },
   },
+  
   solidity: '0.8.20',
   paths: {
     sources: './src',
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
-  },
+  }
 }
+
 
 export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,11 @@ import { productionPublicKeyHash, testPublicKeyHash } from "@anon-aadhaar/core"
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// write all the contract addresses to a json file
+import fs from 'fs'
+
 // ... rest of your Hardhat config
+let testJson = { verifier: '', anonAadhaar: '', gaslessAnonAadhaarCrud: '' }
 
 let publicKeyHash = testPublicKeyHash
 // To deploy contract with production UIDAI public key, will verify real Aadhaar
@@ -13,34 +17,39 @@ if (process.env.PRODUCTION_KEY === 'true') {
   publicKeyHash = productionPublicKeyHash
 }
 
+
 async function main() {
-  // const verifier = await ethers.deployContract('Verifier')
-  // await verifier.waitForDeployment()
+  const verifier = await ethers.deployContract('Verifier')
+  await verifier.waitForDeployment()
 
-  // const _verifierAddress = await verifier.getAddress()
+  const _verifierAddress = await verifier.getAddress()
+  testJson.verifier = _verifierAddress
+  
 
-  // console.log(`Verifier contract deployed to ${_verifierAddress}`)
+  console.log(`Verifier contract deployed to ${_verifierAddress}`)
 
-  // const anonAadhaar = await ethers.deployContract('AnonAadhaar', [
-  //   _verifierAddress,
-  //   publicKeyHash,
-  // ])
+  const anonAadhaar = await ethers.deployContract('AnonAadhaar', [
+    _verifierAddress,
+    publicKeyHash,
+  ])
 
-  //  await anonAadhaar.waitForDeployment()
-  //  const _anonAadhaarAddress = await anonAadhaar.getAddress()
+   await anonAadhaar.waitForDeployment()
+   const _anonAadhaarAddress = await anonAadhaar.getAddress()
+  testJson.anonAadhaar = _anonAadhaarAddress
+  console.log(`AnonAadhaar contract deployed to ${_anonAadhaarAddress}`)
 
-  // console.log(`AnonAadhaar contract deployed to ${_anonAadhaarAddress}`)
-
-  const _anonAadhaarAddress = '0x7c8FfCbeDED64B9c39DB4321a113F0e56Bb75539'
+ // const _anonAadhaarAddress = '0x7c8FfCbeDED64B9c39DB4321a113F0e56Bb75539'
   const gaslessAnonAadhaarCrud = await ethers.deployContract('GaslessAnonAadhaarCrud',[
     _anonAadhaarAddress
 ])
 
   await gaslessAnonAadhaarCrud.waitForDeployment()
 
+  testJson.gaslessAnonAadhaarCrud = await gaslessAnonAadhaarCrud.getAddress()
   console.log(
     `GaslessgaslessAnonAadhaarCrud contract deployed to ${await gaslessAnonAadhaarCrud.getAddress()}`,
   )
+  fs.writeFileSync('test.json', JSON.stringify(testJson, null, 2))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -49,3 +58,6 @@ main().catch(error => {
   console.error(error)
   process.exitCode = 1
 })
+
+
+
